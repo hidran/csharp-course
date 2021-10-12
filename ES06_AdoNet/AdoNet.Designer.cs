20630aa5c225b909c15abaf48e36f800dcf665be
Hidran Arias <hidran@gmail.com>
2021-10-12 15:00:51 +0200

    adds System.Data.Sql connection to northwind db

---
+++ ES06_AdoNet/AdoNet.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace ES06_AdoNet
+{
+    partial class AdoNet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaricaProdotto = new System.Windows.Forms.Button();
+            this.dataProductsGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataProductsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CaricaProdotto
+            // 
+            this.CaricaProdotto.Location = new System.Drawing.Point(31, 13);
+            this.CaricaProdotto.Name = "CaricaProdotto";
+            this.CaricaProdotto.Size = new System.Drawing.Size(75, 23);
+            this.CaricaProdotto.TabIndex = 0;
+            this.CaricaProdotto.Text = "Carica Prodotti";
+            this.CaricaProdotto.UseVisualStyleBackColor = true;
+            this.CaricaProdotto.Click += new System.EventHandler(this.CaricaProdotto_Click);
+            // 
+            // dataProductsGridView
+            // 
+            this.dataProductsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataProductsGridView.Location = new System.Drawing.Point(12, 57);
+            this.dataProductsGridView.Name = "dataProductsGridView";
+            this.dataProductsGridView.RowHeadersWidth = 51;
+            this.dataProductsGridView.RowTemplate.Height = 24;
+            this.dataProductsGridView.Size = new System.Drawing.Size(1207, 505);
+            this.dataProductsGridView.TabIndex = 1;
+            // 
+            // AdoNet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 574);
+            this.Controls.Add(this.dataProductsGridView);
+            this.Controls.Add(this.CaricaProdotto);
+            this.Name = "AdoNet";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataProductsGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CaricaProdotto;
+        private System.Windows.Forms.DataGridView dataProductsGridView;
+    }
+}
+
---
+++ ES06_AdoNet/AdoNet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ES06_AdoNet
+{
+    public partial class AdoNet : Form
+    {
+        public AdoNet()
+        {
+            InitializeComponent();
+        }
+
+        private void CaricaProdotto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               // string connStr = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Northwind; Integrated Security= True";
+                SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["strCon"].ConnectionString);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Products", cn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataProductsGridView.DataSource = dt;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($" {ex.Number} {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
