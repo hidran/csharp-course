f0f0d2e2e1e9d1ee652c59e76290c056fd22fae4
Hidran Arias <hidran@gmail.com>
2021-10-14 11:12:08 +0200

    adds static class

---
+++ ES14-STATIC-CLASS/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES14_STATIC_CLASS
+{
+    public static class Utils
+    {
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
